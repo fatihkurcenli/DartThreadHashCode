253b52878648940ca31804fe58b9ee2014672aeb
Fatih <rizelifatih5353@gmail.com>
2021-06-12 17:35:11 +0300

    last

--- bin/main.dart
+++ bin/main.dart
@@ -7,7 +7,6 @@ import 'package:crypto/crypto.dart';
 import 'dart:convert';
 
 Future main() async {
-  //await runFutures();
   await runThreads();
 }
 
@@ -18,7 +17,6 @@ Future runThreads() async {
   var threads = <Thread>[];
   var numOfThreads = 2;
   for (var i = 0; i < numOfThreads; i++) {
-    // var name = String.fromCharCode(65 + i);
     var thread = Thread(() async {
       buffer._hashCode();
     });
@@ -28,8 +26,6 @@ Future runThreads() async {
 
     threads.add(thread);
     threads.add(thread2);
-    //  await thread.start();
-    // await thread2.start();
   }
 
   for (var i = 0; i < numOfThreads; i++) {
@@ -47,19 +43,9 @@ class _BoundedBuffer<T> {
 
   final Lock _lock = Lock();
 
-  ConditionVariable _notEmpty;
-
-  ConditionVariable _notFull;
-
-  _BoundedBuffer() {
-    _notFull = ConditionVariable(_lock);
-    _notEmpty = ConditionVariable(_lock);
-  }
-
   _hashCode() async {
     try {
       var prefix_str = '0' * difficulty;
-
       for (var nonce = 50000000; nonce < 100000000000; nonce++) {
         var text = blockNumber.toString() +
             transactions +
@@ -78,11 +64,11 @@ class _BoundedBuffer<T> {
           print('Bulunan HashDeğeri Değeri -> $new_hash');
           print('--------------------------------------');
 
+          await _lock.release();
           break;
         }
       }
     } finally {
-      await _lock.release();
       var thread2 = Thread(() async {
         _hashCode2();
       });
@@ -116,155 +102,20 @@ class _BoundedBuffer<T> {
           print('Bulunan HashDeğeri Değeri -> $new_hash');
           print('--------------------------------------');
 
+          await _lock.release();
           break;
         }
       }
     } finally {
-      await _lock.release();
       var thread2 = Thread(() async {
         _hashCode();
       });
       var thread = Thread(() async {
         _hashCode2();
       });
-      await thread.start();
+
       await thread2.start();
-    }
-  }
-
-  String _crypToSha2(String text) {
-    var bytes = utf8.encode(text);
-    var digest2 = sha256.convert(bytes);
-    return digest2.toString();
-  }
-
-  String _crypToSha(String text) {
-    var bytes = utf8.encode(text);
-    var digest2 = sha256.convert(bytes);
-    return digest2.toString();
-  }
-
-/*   for (var i = 0; i < numOfThreads; i++) {
-    await threads[i].join();
-  } */
-}
-
-/* library threading.example.example_interleaved_execution;
-
-import 'dart:async';
-
-import 'package:threading/threading.dart';
-import 'package:crypto/crypto.dart';
-import 'dart:convert';
-
-Future main() async {
-  var buffer = _BoundedBuffer();
-  // var threads = <Thread>[];
-  for (var i = 0; i < 12; i++) {
-    var thread = Thread(() async {
-      buffer._hashCode();
-      print('Çalışan Thread ${Thread.current.name}');
-    });
-    var thread2 = Thread(() async {
-      buffer._hashCode2();
-      print('Çalışan Thread ${Thread.current.name}');
-    });
-
-    // threads.add(thread);
-    await thread.start();
-    await thread2.start();
-  }
-
-/*   for (var i = 0; i < 12; i++) {
-    var thread = Thread(() async {
-      await buffer._hashCode2();
-      print('Çalışan Thread ${Thread.current.name}');
-    });
-
-    threads.add(thread);
-    await thread.start();
-  } */
-
-  /*   for (var thread in threads) {
-    await thread.join();
-  }  */
-}
-
-class _BoundedBuffer<T> {
-  var transactions =
-      "Bize her yer Trabzon! Bölümün en yakisikli hocasi Ibrahim Hoca'dir";
-  var blockNumber = 1;
-  var difficulty = 1;
-  var firstHash =
-      '0000000000000000000000000000000000000000000000000000000000000000';
-
-  final Lock _lock = Lock();
-
-  ConditionVariable _notEmpty;
-
-  ConditionVariable _notFull;
-
-  _BoundedBuffer() {
-    _notFull = ConditionVariable(_lock);
-    _notEmpty = ConditionVariable(_lock);
-  }
-
-  _hashCode() async {
-    try {
-      var prefix_str = '0' * difficulty;
-
-      for (var nonce = 50000000; nonce < 100000000000; nonce++) {
-        var text = blockNumber.toString() +
-            transactions +
-            firstHash +
-            nonce.toString();
-        var new_hash = _crypToSha(text);
-
-        if (new_hash.startsWith(prefix_str) &&
-            new_hash[difficulty + 1] != '0') {
-          await _lock.acquire();
-          firstHash = new_hash;
-          blockNumber += 1;
-          difficulty += 1;
-          print('1.Thread - Block Number -> ${blockNumber - 1}');
-          print('Bulunan Nonce Değeri -> $nonce');
-          print('Bulunan HashDeğeri Değeri -> $new_hash');
-          print('--------------------------------------');
-          break;
-        }
-      }
-    } finally {
-      await _lock.release();
-    }
-    // await _hashCode();
-  }
-
-  _hashCode2() async {
-    try {
-      var prefix_str = '0' * difficulty;
-      for (var nonce = 0; nonce < 50000000; nonce++) {
-        var text = blockNumber.toString() +
-            transactions +
-            firstHash +
-            nonce.toString();
-        var new_hash = _crypToSha2(text);
-
-        if (new_hash.startsWith(prefix_str) &&
-            new_hash[difficulty + 1] != '0') {
-          await _lock.acquire();
-          firstHash = new_hash;
-          blockNumber += 1;
-          difficulty += 1;
-          print('2.Thread - Block Number -> ${blockNumber - 1}');
-          print('Bulunan Nonce Değeri -> $nonce');
-          print('Bulunan HashDeğeri Değeri -> $new_hash');
-          print('--------------------------------------');
-          break;
-        }
-      }
-    } finally {
-      await _lock.release();
-      // await _hashCode2();
+      await thread.start();
     }
   }
 
@@ -280,4 +131,3 @@ class _BoundedBuffer<T> {
     return digest2.toString();
   }
 }
- */
